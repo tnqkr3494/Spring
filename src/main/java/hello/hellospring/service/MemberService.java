e939d09364c94e983e5774860231e5b9a3d0ad36
InGwonKang <tnqkr3494@naver.com>
2024-02-27 19:33:10 +0900

    Service

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member) {
+        // 중복회원검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers(){
+       return memberRepository.findAll();
+    }
+
+    // 회원 한명 조회
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->
+                {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+}
